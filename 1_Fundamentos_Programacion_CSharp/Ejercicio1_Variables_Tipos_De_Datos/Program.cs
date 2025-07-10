29b98f98a1a5d6ae6175d723a9ca7796411554c7
YEREMI JOSÉ FELIZ FULGENCIO <yf2022-0110@uce.edu.do>
2025-07-09 20:29:57 -0400

    agregacion de un metodo

--- 1_Fundamentos_Programacion_CSharp/Ejercicio1_Variables_Tipos_De_Datos/Program.cs
+++ 1_Fundamentos_Programacion_CSharp/Ejercicio1_Variables_Tipos_De_Datos/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var calculator = new Calculator();
+int result = calculator.sum(1, 2);
+Console.WriteLine(result);
+
+public class Calculator
+{
+    public int sum(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+
